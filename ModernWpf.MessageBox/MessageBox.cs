f497185010bac789e8c7a0c96ffc5eda736792a6
OpenByte <development.openbyte@gmail.com>
2021-01-30 18:21:21 +0100

    Add ShowAsync methods

--- ModernWpf.MessageBox/MessageBox.cs
+++ ModernWpf.MessageBox/MessageBox.cs
@@ -2,11 +2,14 @@
 using ModernWpf.Controls;
 using System.Linq;
 using System.Windows;
+using System.Threading.Tasks;
 
 namespace ModernWpf {
     public static class MessageBox {
         public static bool EnableLocalization { get; set; } = true;
 
+
+        #region Sync
         public static MessageBoxResult? Show(string messageBoxText) =>
             Show(null, messageBoxText, null, null, null, null);
         public static MessageBoxResult? Show(string messageBoxText, string? caption) =>
@@ -72,6 +75,83 @@ namespace ModernWpf {
 
             return window.Result ?? defaultResult;
         }
+        #endregion Sync
+
+
+        #region Async
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText) =>
+            ShowAsync(null, messageBoxText, null, null, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption) =>
+            ShowAsync(null, messageBoxText, caption, null, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button) =>
+            ShowAsync(null, messageBoxText, caption, button, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, Symbol symbol) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, null);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, SymbolGlyph symbol) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, null);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, MessageBoxImage image) =>
+            ShowAsync(null, messageBoxText, caption, button, image, null);
+        public static Task<MessageBoxResult> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, Symbol symbol, MessageBoxResult defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, Symbol symbol, MessageBoxResult? defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, defaultResult);
+        public static Task<MessageBoxResult> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, SymbolGlyph symbol, MessageBoxResult defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, SymbolGlyph symbol, MessageBoxResult? defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, symbol, defaultResult);
+        public static Task<MessageBoxResult> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, MessageBoxImage image, MessageBoxResult defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, image, defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(string messageBoxText, string? caption, MessageBoxButton button, MessageBoxImage image, MessageBoxResult? defaultResult) =>
+            ShowAsync(null, messageBoxText, caption, button, image, defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText) =>
+            ShowAsync(owner, messageBoxText, null, null, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption) =>
+            ShowAsync(owner, messageBoxText, caption, null, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button) =>
+            ShowAsync(owner, messageBoxText, caption, button, null, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, Symbol symbol) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, SymbolGlyph symbol) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, MessageBoxImage image) =>
+            ShowAsync(owner, messageBoxText, caption, button, image, null);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, string? glyph) =>
+            ShowAsync(owner, messageBoxText, caption, button, glyph, null);
+        public static Task<MessageBoxResult> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, Symbol symbol, MessageBoxResult defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol.ToGlyph(), defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, Symbol symbol, MessageBoxResult? defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol.ToGlyph(), defaultResult);
+        public static Task<MessageBoxResult> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, SymbolGlyph symbol, MessageBoxResult defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol.ToGlyph(), defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, SymbolGlyph symbol, MessageBoxResult? defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, symbol.ToGlyph(), defaultResult);
+        public static Task<MessageBoxResult> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, MessageBoxImage image, MessageBoxResult defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, image.ToSymbol(), defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, MessageBoxImage image, MessageBoxResult? defaultResult) =>
+            ShowAsync(owner, messageBoxText, caption, button, image.ToSymbol(), defaultResult);
+        public static Task<MessageBoxResult> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, string? glyph, MessageBoxResult defaultResult) =>
+            ShowAsyncInternal(owner, messageBoxText, caption, button, glyph, defaultResult);
+        public static Task<MessageBoxResult?> ShowAsync(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, string? glyph, MessageBoxResult? defaultResult) =>
+            ShowAsyncInternal(owner, messageBoxText, caption, button, glyph, defaultResult);
+
+        private static Task<MessageBoxResult> ShowAsyncInternal(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, string? glyph, MessageBoxResult defaultResult) =>
+            ShowAsyncInternal(owner, messageBoxText, caption, button, glyph, defaultResult);
+        private static Task<MessageBoxResult?> ShowAsyncInternal(Window? owner, string messageBoxText, string? caption, MessageBoxButton? button, string? glyph, MessageBoxResult? defaultResult) {
+            var taskSource = new TaskCompletionSource<MessageBoxResult?>(
+#if !NET45
+                TaskCreationOptions.RunContinuationsAsynchronously
+#endif
+            );
+
+            Application.Current.Dispatcher.Invoke(() => {
+                var result = ShowInternal(owner, messageBoxText, caption, button, glyph, defaultResult);
+                taskSource.TrySetResult(result);
+            });
+
+            return taskSource.Task;
+        }
+#endregion Async
+
 
         private static Window? GetActiveWindow() =>
             Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window.IsActive);
