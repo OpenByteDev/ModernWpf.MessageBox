3fd43fcd4a3419d84e8d663914101f910a72179a
wherewhere <qq2518766683@outlook.com>
2022-02-08 15:55:01 +0800

    Now it has Mica

--- ModernWpf.MessageBox/MessageBoxWindow.xaml.cs
+++ ModernWpf.MessageBox/MessageBoxWindow.xaml.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
+using MicaWPF.Controls;
 using DialogBoxCommand = ModernWpf.LocalizedDialogCommands.DialogBoxCommand;
 
-namespace ModernWpf {
-    public partial class MessageBoxWindow : Window {
+namespace ModernWpf
+{
+    public partial class MessageBoxWindow : MicaWindow
+    {
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
         public MessageBoxResult? Result = null;
 
         public MessageBoxWindow(string messageBoxText, string caption, MessageBoxButton button, string? symbolGlyph) {
@@ -87,6 +99,12 @@ namespace ModernWpf {
             base.OnSourceInitialized(e);
 
             InvalidateMeasure();
+        }
+
+        private void MicaWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
         }
     }
 }
