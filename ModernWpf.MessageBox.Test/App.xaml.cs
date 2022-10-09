f329bbadc4c2614d15d08edb176fb0a82630678b
wherewhere <qq2518766683@outlook.com>
2022-10-09 18:10:04 +0800

    Make it as a ContentDialog

--- ModernWpf.MessageBox.Test/App.xaml.cs
+++ ModernWpf.MessageBox.Test/App.xaml.cs
@@ -13,18 +13,18 @@ namespace ModernWpfMessageBox.Test
             string title = "Some title";
             string message = "This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong test text!";
 
-            // MessageBox.Show(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
-            // MessageBox.Show("adawdawda", title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            System.Windows.MessageBox.Show(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            System.Windows.MessageBox.Show("adawdawda", title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
             ShutdownMode = ShutdownMode.OnExplicitShutdown;
-            // ModernWpf.MessageBox.Show("This is a test text!", "Some title", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
-            // ModernWpf.MessageBox.Show(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
-            ModernWpf.MessageBox.Show("redadwada", null, MessageBoxButton.OK, Symbol.Admin);
-            ModernWpf.MessageBox.Show("redadwada", null, MessageBoxButton.OK, SymbolGlyph.Airplane);
-            ModernWpf.MessageBox.Show("redadwada", null, MessageBoxButton.OK, SymbolGlyph.Airplane, MessageBoxResult.OK);
-            ModernWpf.MessageBox.ShowAsync(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question).GetAwaiter().GetResult();
-            ModernWpf.MessageBox.ShowAsync(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Hand, MessageBoxResult.Cancel).GetAwaiter().GetResult();
-            ModernWpf.MessageBox.EnableLocalization = false;
-            ModernWpf.MessageBox.ShowAsync("Press Alt and you should see underscores!", null, MessageBoxButton.YesNoCancel, MessageBoxImage.Hand).GetAwaiter().GetResult();
+            ModernWpf.Controls.MessageBox.Show("This is a test text!", "Some title", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            ModernWpf.Controls.MessageBox.Show(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            ModernWpf.Controls.MessageBox.Show("redadwada", null, MessageBoxButton.OK, ((char)Symbol.Admin).ToString());
+            ModernWpf.Controls.MessageBox.Show("redadwada", null, MessageBoxButton.OK, SymbolGlyph.Airplane);
+            ModernWpf.Controls.MessageBox.Show("redadwada", null, MessageBoxButton.OK, SymbolGlyph.Airplane, MessageBoxResult.OK);
+            ModernWpf.Controls.MessageBox.ShowAsync(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question).GetAwaiter().GetResult();
+            ModernWpf.Controls.MessageBox.ShowAsync(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Hand, MessageBoxResult.Cancel).GetAwaiter().GetResult();
+            ModernWpf.Controls.MessageBox.EnableLocalization = false;
+            ModernWpf.Controls.MessageBox.ShowAsync("Press Alt and you should see underscores!", null, MessageBoxButton.YesNoCancel, MessageBoxImage.Hand).GetAwaiter().GetResult();
             Shutdown();
         }
     }
